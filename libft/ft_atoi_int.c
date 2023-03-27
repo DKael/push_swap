82335a0baf42dcc12a5991b2bd45a48e9e3c139d
Hyungdo Kim <hyungdki@c2r7s4.42seoul.kr>
2023-03-27 13:51:33 +0900

    main input validation

---
+++ libft/ft_atoi_int.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungdki <hyungdki@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 16:32:23 by hyungdki          #+#    #+#             */
+/*   Updated: 2022/11/21 16:32:24 by hyungdki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <limits.h>
+
+int	ft_atoi_int(const char *str)
+{
+	int			idx;
+	int			sign;
+	long long	nb;
+
+	idx = 0;
+	sign = 1;
+	nb = 0;
+	while ((9 <= str[idx] && str[idx] <= 13) || str[idx] == ' ')
+		idx++;
+	if (str[idx] == '-' || str[idx] == '+')
+		if (str[idx++] == '-')
+			sign = -1;
+	while ('0' <= str[idx] && str[idx] <= '9')
+	{
+		if (nb > INT_MAX / 10 
+			|| (nb == (INT_MAX / 10) && str[idx] - '0' > (INT_MAX % 10) - ((sign - 1) / 2)))
+		{
+			if (sign == 1)
+				return (0);
+			else
+				return (-1);
+		}
+		nb = nb * 10 + (str[idx++] - '0');
+	}
+	return ((int)nb * sign);
+}
--- main.c
+++ main.c
@@ -4,34 +4,133 @@
 #include "libft/ft_split.c"
 #include "libft/ft_strdup.c"
 #include "libft/ft_strlen.c"
+#include "libft/ft_atoi_int.c"
 #include <unistd.h>
 #include <stdlib.h>
 
+void array_free(int argc, char ***argv_split);
+
 #include <stdio.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	int		argv_index;
-	int		split_index;
-	int		input_count;
-	char	***argv_split_result;
-	int		*sorted_input;
+	int argv_idx;
+	int split_idx;
+	int idx;
+	int input_count;
+	char ***argv_split;
+	int *sorted_input;
+	int sorted_idx;
+	char temp;
 
 	if (argc != 1)
 	{
-		argv_index = 0;
-		argv_split_result = (char***)malloc(sizeof(char**) * (argc - 1));
-		while (++argv_index < argc)
-			argv_split_result[argv_index - 1] = ft_split(argv[argv_index], ' ');
-		argv_index = -1;
-		input_count = 0;
-		while (++argv_index < argc - 1)
+		// argument split
+		argv_split = (char ***)malloc(sizeof(char **) * (argc - 1));
+		if (argv_split == NULL)
+			exit(1);
+		argv_idx = 0;
+		while (++argv_idx < argc)
 		{
-			split_index = -1;
-			while (argv_split_result[argv_index][++split_index] != '\0')
+			argv_split[argv_idx - 1] = ft_split(argv[argv_idx], ' ');
+			if (argv_split[argv_idx - 1] == NULL)
+			{
+				while (--argv_idx != 0)
+					free(argv_split[argv_idx - 1]);
+				free(argv_split);
+				exit(1);
+			}
+		}
+		// check non numeric character
+		argv_idx = -1;
+		while (++argv_idx < argc - 1)
+		{
+			split_idx = -1;
+			while (argv_split[argv_idx][++split_idx] != NULL)
+			{
+				idx = 0;
+				temp = argv_split[argv_idx][split_idx][idx];
+				if (temp == '-' || temp == '+')
+				{
+					temp = argv_split[argv_idx][split_idx][idx + 1];
+					if (temp == '\0')
+					{
+						write(1, "Error", 5);
+						array_free(argc, argv_split);
+						exit(1);
+					}
+				}
+				while (argv_split[argv_idx][split_idx][++idx] != '\0')
+				{
+
+					if ('0' > temp || temp > '9')
+					{
+						write(1, "Error", 5);
+						array_free(argc, argv_split);
+						exit(1);
+					}
+				}
+			}
+		}
+		// count total numeric input
+		argv_idx = -1;
+		input_count = 0;
+		while (++argv_idx < argc - 1)
+		{
+			split_idx = -1;
+			while (argv_split[argv_idx][++split_idx] != NULL)
 				input_count++;
 		}
+		// make array use to sorting input
+		sorted_input = (int *)malloc(sizeof(int) * input_count);
+		if (sorted_input == NULL)
+		{
+			array_free(argc, argv_split);
+			exit(1);
+		}
+		argv_idx = -1;
+		sorted_idx = -1;
+		while (++argv_idx < argc - 1)
+		{
+			split_idx = -1;
+			while (argv_split[argv_idx][++split_idx] != NULL)
+			{
+				sorted_input[++sorted_idx] = ft_atoi_int(argv_split[argv_idx][split_idx]);
+				if ((sorted_input[sorted_idx] == 0 && argv_split[argv_idx][split_idx][0] != '0') 
+					|| (sorted_input[sorted_idx] == -1 && argv_split[argv_idx][split_idx][0] != '-' 
+					&& argv_split[argv_idx][split_idx][1] != '1'))
+				{
+					write(1, "Error", 5);
+					free(sorted_input);
+					array_free(argc, argv_split);
+					exit(1);
+				}
+			}
+		}
+		for (int i = 0; i < input_count; i++)
+		{
+			printf("%d ", sorted_input[i]);
+		}
+		// array sorting
 
-		printf("input count : %d\n", input_count);
+		// list free part
+		free(sorted_input);
+		array_free(argc, argv_split);
 	}
+}
+
+void array_free(int argc, char ***argv_split)
+{
+	int argv_idx;
+	int split_idx;
+
+	argv_idx = -1;
+	while (++argv_idx < argc - 1)
+	{
+		split_idx = -1;
+		while (argv_split[argv_idx][++split_idx] != NULL)
+			free(argv_split[argv_idx][split_idx]);
+		free(argv_split[argv_idx]);
+	}
+	free(argv_split);
 }
